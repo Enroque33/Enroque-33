836bae182140e4a35173adc0a5c1474dcf7d03cd
Enroque33 <matiasdomini@gmail.com>
2026-01-04 21:32:37 -0300

    Add files via upload

--- productos.js
+++ productos.js
@@ -1,5 +1,5 @@
 const productos = [
   { id: 1, nombre: "teclado", precio: 500 },
-  { id: 2, nombre: "termo", precio: 1200 },
+  { id: 2, nombre: "termo", precio: 1300 },
   { id: 3, nombre: "Producto C", precio: 800 }
 ];
